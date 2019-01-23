0222e1c4e8a2e589820cabba501aa290886ac903
dorisxu <dorisoks@gmail.com>
2019-01-23 00:05:47 -0800

    add producer

---
+++ kafka/producer.py
@@ -0,0 +1,23 @@
+from kafka import KafkaProducer, KafkaConsumer
+import time, sys
+from time import gmtime, strftime
+
+
+def main():
+    producer = KafkaProducer(bootstrap_servers='ec2-54-201-89-215.us-west-2.compute.amazonaws.com:9092')
+    
+    file_address = "/home/ubuntu/input.txt"
+
+    count = 0
+    with open(file_address) as f:
+        for line in f:
+            curtime = strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+            new_line = line + " " + curtime
+            producer.send("ctest", new_line)
+            count += 1
+	    print("now it is sending... " + new_line)
+    producer.flush()
+    f.close()
+
+if __name__ == "__main__":
+    main()
