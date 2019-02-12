65e744511a4c76cdaa388330152a12414cf2c6cf
dorisoks <44152373+dorisoks@users.noreply.github.com>
2019-02-11 17:27:43 -0800

    Delete run_test_ksc.sh

--- processing/batch/run_test_ksc.sh
+++
@@ -1,3 +0,0 @@
-/usr/local/spark/bin/spark-submit \
---packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2,TargetHolding/pyspark-cassandra:0.3.5 \
---conf spark.cassandra.connection.host=52.32.25.168 test_kafka_spark_cassandra.py ctest2
